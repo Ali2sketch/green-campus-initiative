580f81a7a9e10ba5b267f645ccce926ea5b824ac
Ali <alialsisimail@gmail.com>
2026-05-19 18:11:11 +0300

    Add save paper function to points system

--- points.js
+++ points.js
@@ -5,3 +5,7 @@ function recycle() {
     points += 10;
     console.log('+10 points for recycling! Total: ' + points);
 }
+function savePaper() {
+    points += 5;
+    console.log('+5 points for saving paper! Total: ' + points);
+}
